48b2c48368c67e04f0be2a3b428eb4cd43f1952c
muhammed-bayat <muhammed.bayat07@erzurum.edu.tr>
2022-03-31 15:57:37 +0300

    Completed Teams Model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from pages.models import Team
+from django.utils.html import format_html
+
+
+
+
+
+
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, obj):
+        if obj.photo:
+            return format_html('<img src="{url}" width="50" height="50" />'.format(url=obj.photo.url))
+        else:
+            return "-"
+
+
+    list_display = ('id','thumbnail', 'first_name', 'designation',"created_date")
+    list_display_links = ('id', 'first_name', 'designation')
+    list_filter = ('designation',)
+    search_fields = ('first_name', 'designation')
+    list_per_page = 25
+
+
+
+
+admin.site.register(Team,TeamAdmin)
